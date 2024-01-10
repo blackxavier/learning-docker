d8442cf3d1e8ef4105799bcf0642e1d804402309
blackxavier <ogbuchiarinze01@gmail.com>
2024-01-10 22:10:57 +0100

    Untrack everything

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -32,7 +32,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_prometheus",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -42,6 +44,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # ...
+    "django_prometheus.middleware.PrometheusBeforeMiddleware",
+    # ...
+    "django_prometheus.middleware.PrometheusAfterMiddleware",
 ]
 
 ROOT_URLCONF = "hello_django.urls"
